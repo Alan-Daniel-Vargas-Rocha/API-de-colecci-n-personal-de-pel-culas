9b1112a1c14abbdea742ca3f39d9b6fd94f74289
FelJR04 <quezadamaldonadofelipe@gmail.com>
2026-07-14 19:58:28 -0700

    Agregar interfaz inicial del catalogo

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from src.routes import pelicula_apy, coleccion_apy, usuario_apy,coleccion_pelicula_apy
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()  
 
-@app.get("/")
-def read_root():
-    return {""}
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static"
+)
+
+@app.get("/", include_in_schema=False)
+def mostrar_inicio():
+    return FileResponse(
+        BASE_DIR / "templates" / "index.html"
+    )
 
 app.include_router(pelicula_apy.router)
 app.include_router(coleccion_apy.router)
